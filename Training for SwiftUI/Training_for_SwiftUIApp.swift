6d07d092e6b7e1e95f53e8fd55c6bf27af7f5bad
motoki0724 <motokiokayasu@gmail.com>
2023-09-17 09:12:54 +0900

    Initial Commit

---
+++ Training for SwiftUI/Training_for_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_for_SwiftUIApp.swift
+//  Training for SwiftUI
+//
+//  Created by Motoki Okayasu on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct Training_for_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
