cd84d5a54de3cc90f4fba6a70d6047cabef190e4
motoki0724 <motokiokayasu@gmail.com>
2023-09-17 11:27:04 +0900

    Lets make it Humberger

--- Training for SwiftUI/ContentView.swift
+++ Training for SwiftUI/ContentView.swift
@@ -7,56 +7,34 @@
 
 import SwiftUI
 
+//Lets make it Humburger!!!
 struct ContentView: View {
     var body: some View {
         VStack {
             Rectangle()
-                .foregroundColor(.gray)
-                .frame(width: 350, height: 100)
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
             Rectangle()
-                .foregroundColor(.purple)
-                .frame(width: 350, height: 100)
-            HStack {
-                Rectangle()
-                    .foregroundColor(.blue)
-                    .frame(width: 150, height: 150)
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 100, height: 100)
-                Rectangle()
-                    .foregroundColor(.yellow)
-                    .frame(width: 50, height: 50)
-            }
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.green)
-                    .frame(width: 350, height: 200)
-                HStack {
-                    VStack {
-                        Rectangle()
-                            .foregroundColor(.pink)
-                            .frame(width: 150, height: 70)
-                        Rectangle()
-                            .foregroundColor(.black)
-                            .frame(width: 150, height: 70)
-                    }
-                    VStack {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .frame(width: 150, height: 70)
-                        Rectangle()
-                            .foregroundColor(.yellow)
-                            .frame(width: 150, height: 70)
-                    }
-                }
-            }
+                .foregroundColor(.red)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.yellow)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.brown)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.green)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
     }
 }
